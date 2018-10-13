f7d20788063f9aef529f5efe043e1b276828bb53
William Rodriguez <wrod@mit.edu>
2018-10-13 14:36:07 -0400

    Added functionality for guest check-in status; still buggy

--- js/guestListServer.js
+++ js/guestListServer.js
@@ -1,55 +1,101 @@
-var guests = {"Ali":["Jack","John","France"],"Ana":["Kat","Joe","Julia"]};
+var guestsByMember = {"Ali":["Jack","John","France"],"Ana":["Kat","Joe","Julia"]};
+
+var guestsByMemberWithState = {};
+var defaultInitialGuestCount = 0;
+
+var currentGuestCount = defaultInitialGuestCount;
 
 
-function createGuestDataStructure(guests){
-	console.log("testing");	
+function checkInMembersGuest(member,guestname){
+	console.log("member:",member);
+	console.log("guestname:",guestname);
+
+	membersGuests = guestsByMemberWithState[member];
+	membersGuests[guestname].checkedin = true;
+}
+
+function createGuestDataStructure(guestsDict){
+	console.log(guestsDict);
+	var members = Object.keys(guestsDict);
+	for (var i =0; i <members.length; i++){
+		member = members[i];
+		membersGuests = guestsByMember[member];
+		guestsWithStates = {};
+		for (var j=0; j <membersGuests.length;j++){
+			guest = membersGuests[j];
+			guestsWithStates[guest] = {"name": guest, "checkedin" : false};
+		}
+		guestsByMemberWithState[member] = guestsWithStates;
+	}
 }
 
 
 window.addEventListener('DOMContentLoaded', function(){
-	populateGuestList();
-	document.getElementById()
+	createGuestDataStructure(guestsByMember);
 
 });
 
 
+function incrementGuestCount(){
+	currentGuestCount++;
+	document.getElementById("currentGuestCount").innerHTML = currentGuestCount; 
+}
 
-// Util.events(document, {
-// 	// Final initalization entry point: the Javascript code inside this block
-// 	// runs at the end of start-up when the DOM is ready
+function decrementGuestCount(){
+	if (currentGuestCount != 0){
+		currentGuestCount--;
+		document.getElementById("currentGuestCount").innerHTML = currentGuestCount; 
+	}
+}
 
-// 	"DOMContentLoaded": function() {
 
-// 		// Add events
-// 		rules.prepareNewGame();
 
-// 		// Captures effects and logic when user presses down on mouse.
-// 		Util.one("#pageBody").addEventListener("mousedown", function( event){
+function clearGuestList(){
+	document.getElementById("guestList").innerHTML = "";	
 
-// 		},false);
-// 		// Captures effects and logic when user presses up (releases) mouse.
-// 		Util.one("#pageBody").addEventListener("mouseup", function( event){
-	
-// 		},false);
-// 		// Apply logic for moving mouse throughout grid
-// 		Util.one("#pageBody").addEventListener("mousemove", function( event){
+}
 
-// 		},false);
-// 		// Apply logic for when mouse leaves page
-// 		Util.one("#pageBody").addEventListener("mouseleave", function( event ) {
-	
-// 		}, false);
-// 	},
+function populateGuestsOfMember(member){
+	clearGuestList();
 
-// 	// Keyboard events arrive here
-// 	"keydown": function(evt) {
-// 	},
+	guests = Object.keys(guestsByMemberWithState[member]);
+	for (var i =0; i <guests.length; i++){
+		guestObject = guestsByMemberWithState[member][guests[i]];
+		name = guestObject['name'];
+		checkedin = guestObject['checkedin'];
 
-// 	// Click events arrive here
-// 	"click": function(evt) {
-// 	}
-// });
 
+		var guestHTML = document.createElement("a");
+		var guestHTMLClass= "list-group-item list-group-item-action";
+		guestHTML.setAttribute("class",guestHTMLClass);
+		guestHTML.setAttribute("href","#");
+		guestHTML.innerHTML = name;
+
+
+		var buttonHTML = document.createElement("button");
+		buttonHTML.setAttribute("type","button");
+		buttonHTML.setAttribute("class","btn btn-primary");
+		buttonHTML.addEventListener("click", function (){
+			checkInMembersGuest(member,name);
+		});
+
+		if (!checkedin){
+			buttonHTML.classList.add("btn-success");
+			buttonHTML.innerHTML = "Check-in";
+
+		}
+		else{
+			buttonHTML.classList.add("disabled");
+			buttonHTML.innerHTML = "Checked-in"
+
+		}
+		guestHTML.appendChild(buttonHTML);
+		console.log(guestHTML);
+		
+		document.getElementById("guestList").appendChild(guestHTML);	
+	}
+
+}
 
 
 
@@ -65,7 +111,16 @@ function populateGuestList(){
 	buttonHTML.setAttribute("type","button");
 	buttonHTML.setAttribute("class","btn btn-primary");
 	buttonHTML.innerHTML = "Check-in";
+	buttonHTML.addEventListener("mouseup", function(event){
+		buttonHTML.classList.remove("btn-primary");
+		buttonHTML.innerHTML = "Checked In";
+		buttonHTML.classList.add("btn-success");
+		buttonHTML.classList.add("disabled");
+
+	},false);
+
 	guestHTML.appendChild(buttonHTML);
+
 
 	document.getElementById("guestList").appendChild(guestHTML);	
 
